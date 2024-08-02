0c5133603e4fc6e1183e4ebefee417a3c8ae24af
Rizky Syahri Ramadhan <ramadhancai@gmail.com>
2024-08-02 17:29:44 +0700

    updated category

--- src/category/category.controller.ts
+++ src/category/category.controller.ts
@@ -1,12 +1,41 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
 import { CategoryService } from './category.service';
+import { AuthGuard } from 'src/guards/auth.guard';
+import { CategoryDto } from './dto/create-category.dto';
 
 @Controller('category')
 export class CategoryController {
   constructor(private categoryService: CategoryService) {}
 
+  @UseGuards(AuthGuard)
   @Get()
-  async findCategory() {
-    return await this.categoryService.findCategory();
+  async findCategory(@Req() req) {
+    const userId = req.userId;
+    return await this.categoryService.findCategory(userId);
+  }
+
+  @UseGuards(AuthGuard)
+  @Post()
+  async craeteCategory(@Body() categoryDto: CategoryDto, @Req() req) {
+    const userId = req.userId;
+
+    return await this.categoryService.createCategory(categoryDto, userId);
+  }
+
+  @UseGuards(AuthGuard)
+  @Delete(':id')
+  async deleteCategoruById(@Param('id') id: string, @Req() req) {
+    const userId = req.userId;
+
+    return this.categoryService.deleteCategoryById(id, userId);
   }
 }
--- src/category/category.service.ts
+++ src/category/category.service.ts
@@ -1,11 +1,49 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { PrismaService } from 'src/prisma.service';
+import { CategoryDto } from './dto/create-category.dto';
 
 @Injectable()
 export class CategoryService {
   constructor(private prisma: PrismaService) {}
 
-  async findCategory() {
-    return await this.prisma.category.findMany();
+  async findCategory(userId: string) {
+    return await this.prisma.category.findMany({
+      where: {
+        userId,
+      },
+    });
+  }
+
+  async createCategory(categoryData: CategoryDto, userId: string) {
+    const category = await this.prisma.category.create({
+      data: {
+        name: categoryData.name,
+        userId,
+      },
+    });
+
+    return category;
+  }
+
+  async deleteCategoryById(id: string, userId: string) {
+    const category = await this.prisma.category.findUnique({
+      where: { id },
+    });
+
+    if (!category) {
+      throw new NotFoundException(`Category with ID ${id} not found`);
+    }
+
+    if (category.userId !== userId) {
+      throw new Error(`User is not authorized to delete this category`);
+    }
+
+    await this.prisma.category.delete({
+      where: { id },
+    });
+
+    return {
+      message: `Category with id ${id} has beel deleted`,
+    };
   }
 }
